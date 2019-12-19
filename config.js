72e1e1f21709046541c5a8eb74b01f3818bcd656
Showmen Talukder <38407270+SoulSen@users.noreply.github.com>
2019-12-18 18:25:48 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Obscure 5.0.1.",
+   "version": "5.0.1",
+   "homepage": "https://nodeunblocker-soul.herokuapp.com/",
+   "enableLogoutBttn": true,
+   "kioskEnabled": true
+};
